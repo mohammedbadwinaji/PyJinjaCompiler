82585a331ad56cad08f86bca93b3d0bfb121f019
mohammedbadwinaji <mohammedbadwinaji@gmail.com>
2026-08-13 18:46:49 +0300

    no

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import test.CompilerPipelineTest;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-//        CompilerPipelineTest test =new CompilerPipelineTest();
-//        test.test("test-data/python/test01.py","test-data/jinja");
+        CompilerPipelineTest test =new CompilerPipelineTest();
+        test.test("test-data/python/test01.py","test-data/jinja");
 
-        Compiler.run("test-data/python/compiler_full_test.py","test-data/jinja");
+//        Compiler.run("test-data/python/test01.py","test-data/jinja");
     }
 }
